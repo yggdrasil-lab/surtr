cef6cf45176850f5b5b44da31dc559f97faf1ced
Raymund <mon2x0@gmail.com>
2026-01-02 20:52:39 +1300

    feat: Introduce `scripts` submodule and refactor deployment to use `scripts/deploy.sh`.

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
-# Deploy to Swarm
-docker stack deploy -c docker-compose.yml surtr
+set -e
+# Usage: ./scripts/deploy.sh <STACK_NAME> [COMPOSE_FILES...]
+./scripts/deploy.sh "surtr" docker-compose.yml
